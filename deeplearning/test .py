bc7cec5f329689917321fe50ad257c050d8a47c7
chickchick1044 <chickchick1044@naver.com>
2019-07-19 15:28:44 +0900

    deeplearning

---
+++ deeplearning/test .py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# !/usr/bin/python3
+# python test.py
+
+test = ['test', 'test1']
+test2 = 'test'
+test3 = "test"
+test4 = "test"
+'''
+Multiple Comment
+'''
+input('\n\nTest Input')
+import sys; x='test'; print(x)
+
+
+# In[15]:
+
+
+# Variable Types
+counter = 100
+miles = 1000.0
+name = "Tom"
+print(counter)
+print(miles)
+print(name)
+
+a = b = c = 1
+a, b, c = 1, 2, 3
+
+# Standard Data Types
+# Numbers
+# String
+# List
+# Tuple
+# Dictionary
+
+# Numbers
+var1 = 1
+var2 = 20
+var3 = 30
+
+# del var1
+# del var2
+del var2, var3
+
+# Numerical Types -> int, float, complex
+# int->10, float->0.0 #complex->3.14j
+
+# String 
+str = "Hello Python"
+print(str)
+print(str[0]) # First charactor of the string
+print(str[2:5])
+print(str[2:])
+print(str * 2)
+print(str + "Test")
+str[:-1]
+
+# List
+list = ['abcd', 'a', 10, 23.1, 'Tom']
+print(list)
+print(list[0])
+print(list[1:3])
+
+list2 = ['apple'+'orange']
+print(list+list2)
+
+# Tuple
+tuple = (1, "2", "test")
+print(tuple)
+# tuple[0] = 100
+print(tuple[0])
+print(tuple[1:2])
+
+# Dictionary
+dict= {}
+dict['one'] = "This is one"
+print(dict)
+print(dict.keys())
+print(dict.values())
+var12 = 20
+chr(var12)
+#set, dict, str, int, float, repr:객체를 스트링으로
+
+
+# In[16]:
+
+
+# Basic Operators
+# Arithmetic Operator: + - * / % **
+# Comparison Operator : =, !=, >,M, >=, <=
+# Assignment Operator : =, +=, -=, *=, **=
+# Logical Operator : and, or, not, 
+# Bitwise Operator
+# Membership Operator : in, not in
+# identity Operator
+
+
+# In[23]:
+
+
+#Decision Making
+var10 = 100
+if var10:
+    print("10")
+    print(var10)
+    
+var11=11
+if var11:
+    print(1)
+else:
+    print("not 11")
+    
+amount = int(input() )
+if amount < 1000:
+    discount = amount * 0.05
+    print("Discount", discount)
+else:
+    discount = amount * 0.10
+    print("Discount", discount)
+    
+    
+amount2 = 0
+if amount2 < 1000:
+    discount = amount2 * 0.05
+    print("Discount", discount)
+elif amount2 < 5000:
+    discount = amount2 * 0.10
+    print("Discount", discount)
+else:
+    discount = amount * 0.10
+    print("Discount", discount)
+
+
+# In[ ]:
+
+
+# Loop
+count = 0
+while(count<3):
+    print("Count", count)
+    count = count+1
+print("End")
+
+var = 1
+while var ==1:
+    num = int(input("Enter a number: "))
+    print("You entered ", num)
+    
+
+
+# In[ ]:
+
+
+count = 0
+while count < 5:
+    print(count, "is less that 5")
+    count = count + 1
+    
+if 
+else:
+    print(count, "This is more than 6")
+
+
+# In[1]:
+
+
+#!/usr/bin/env python
+#!/usr/bin/python -> test.py
+#usr/bin/python -> python test.py
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
